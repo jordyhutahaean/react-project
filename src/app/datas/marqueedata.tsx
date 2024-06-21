244bf070c5c78efc9e019f6a09bac8a6751809a8
giogio555 <jordyhutahaean555@gmail.com>
2024-06-21 16:50:11 +0700

    init commit

--- src/app/datas/marqueedata.tsx
+++ src/app/datas/marqueedata.tsx
@@ -1,5 +1,5 @@
-import Marquee from "../components/utils/marquee";
-import {cn} from "../components/lib/cn"
+import Marquee from "../../components/magicui/marquee";
+import {cn} from "../../lib/utils"
 
 
 const reviews = [
@@ -50,34 +50,34 @@ const reviews = [
     username,
     body,
   }: {
-    img: string;
-    name: string;
-    username: string;
-    body: string;
-  }) => {
-    return (
-      <figure
-        className={cn(
-          "relative w-64 cursor-pointer overflow-hidden rounded-xl border p-4",
-          // light styles
-          "border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]",
-          // dark styles
-          "dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]",
-        )}
-      >
-        <div className="flex flex-row items-center gap-2">
-          <img className="rounded-full" width="32" height="32" alt="" src={img} />
-          <div className="flex flex-col">
-            <figcaption className="text-sm font-medium dark:text-white">
-              {name}
-            </figcaption>
-            <p className="text-xs font-medium dark:text-white/40">{username}</p>
-          </div>
-        </div>
-        <blockquote className="mt-2 text-sm">{body}</blockquote>
-      </figure>
-    );
-  };
+      img: string;
+      name: string;
+      username: string;
+      body: string;
+      }) => {
+        return (
+          <figure
+            className={cn(
+              "relative w-64 cursor-pointer overflow-hidden rounded-xl border p-4",
+              // light styles
+              "border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]",
+              // dark styles
+              "dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]",
+            )}
+          >
+            <div className="flex flex-row items-center gap-2">
+              <img className="rounded-full" width="32" height="32" alt="" src={img} />
+              <div className="flex flex-col">
+                <figcaption className="text-sm font-medium dark:text-white">
+                  {name}
+                </figcaption>
+                <p className="text-xs font-medium dark:text-white/40">{username}</p>
+              </div>
+            </div>
+            <blockquote className="mt-2 text-sm">{body}</blockquote>
+          </figure>
+        );
+      };
    
   const MarqueeDemo = () => {
     return (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import Image from "next/image";
 import MarqueeDemo from "./datas/marqueedata";
+import BentoDemo from "./datas/bentodata"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 overflow-hidden ">
-      <div className="content1">
-        <section className="part1 flex flex-col items-center justify-center">
-          <h1 className="flex-col justify-center items-center py-40"> Hello Everynyan</h1>
+      <div className="content1 w-full">
+        <section className="part1 flex flex-col items-center justify-center w-full">
+          <h1 className="flex-col justify-center items-center "> Hello Everynyan</h1>
           <Image
-                 className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+                 className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert py-10"
+                 
                  src="/next.svg"
                  alt="Next.js Logo"
                  width={180}
@@ -17,8 +19,16 @@ export default function Home() {
                  >
                  </Image>       
         </section>
+
+        <div className="w-full">
+          <BentoDemo/>
+        </div>
+
+        <div className="w-full py-10">
+          <MarqueeDemo/>
+        </div>
+
         
-        <MarqueeDemo/>
       </div>
     </main>
   );
